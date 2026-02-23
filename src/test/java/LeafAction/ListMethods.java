d28b4731239dbe6cea897641b7f4e3c757621b93
vikramsreenivasulu <vikramsreenivasulu16@gmail.com>
2026-02-23 16:07:36 +0530

    23/2/2026

--- src/test/java/LeafAction/ListMethods.java
+++ src/test/java/LeafAction/ListMethods.java
@@ -1,9 +1,8 @@
 package LeafAction;
 
-import org.openqa.selenium.interactions.Actions;
-
 import LeafLocators.ListLocators;
-import Utility.BaseClass;
+import LeafLocators.ListLocators.Menu.MenuBar;
+import LeafLocators.ListLocators.Menu.TabMenu;
 
 public class ListMethods {
 
@@ -11,12 +10,14 @@ public class ListMethods {
 	
 	public Menu menu;
 	
+	
 	public ListLocators listlocators = new ListLocators(); 
 	
 	
 	public ListMethods() {
 		listmethod = new ListMethod();
 		menu = new Menu();
+		
 	}
 	
 	
@@ -27,46 +28,51 @@ public class ListMethods {
 		}
 	}
 	
-	public class Menu{
+	public class Menu {
 		
 		public void ClickMenu() {
 			listlocators.menu.ClickMenu.click();
 		}
 		
-		public void MenuBarCustomers() {
+		
 			
-			listlocators.menu.menubar.MenuBarCustomers.click();
+			public void MenuBarCustomers() {
+				listlocators.menu.menubar.MenuBarCustomers.click();
+			}
 			
-		}
-		
-		public void MenuBarOrders() {
-			listlocators.menu.menubar.MenuBarOrders.click();
+			public void MenuBarOrders() {
+				listlocators.menu.menubar.MenuBarOrders.click();
+			}
 			
-		}
+			public void MenuBarShipments() {
+				listlocators.menu.menubar.MenuBarShipments.click();
+			}
+			
+			public void MenuBarProfile() {
+				listlocators.menu.menubar.MenuBarProfile.click();
+			}
 		
-		public void MenuBarShipments() {
-			listlocators.menu.menubar.MenuBarShipments.click();
-		}
 		
-		public void MenuBarProfile() {
-			listlocators.menu.menubar.MenuBarProfile.click();
-		}
 		
-		public void TabMenuOverview() {
-			listlocators.menu.tabmenu.TabMenuOverview.click();
-		}
+			
+			
+			public void TabMenuOverview() {
+				listlocators.menu.tabmenu.TabMenuOverview.click();
+			}
+			
+			public void TabMenuMembers() {
+				listlocators.menu.tabmenu.TabMenuMembers.click();
+			}
+			
+			public void TabMenuSales() {
+				listlocators.menu.tabmenu.TabMenuSales.click();
+			}
+			
+			public void TabMenuProfile() {
+				listlocators.menu.tabmenu.TabMenuProfile.click();
+			}
 		
-		public void TabMenuMembers() {
-			listlocators.menu.tabmenu.TabMenuMembers.click();
-		}
 		
-		public void TabMenuSales() {
-			listlocators.menu.tabmenu.TabMenuSales.click();
-		}
-		
-		public void TabMenuProfile() {
-			listlocators.menu.tabmenu.TabMenuProfile.click();
-		}
 	}//EndMenu
 
 	
